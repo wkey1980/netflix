38af267f4d99d42df50812ebe347a89f6d5ee21b
Will <28814921+wkey1980@users.noreply.github.com>
2021-06-08 17:14:05 +0100

    Added base styles to globalStyles.js

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,8 +1,21 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    background-color: teal;
+  * {
+      outline: 1px solid red;
+  }
+
+  html {
+      font-size: 100%;
+      box-sizing: border-box;
+  }
+
+  *,
+  *::before,
+  *::after {
+      box-sizing: inherit;
+      margin: 0;
+      padding: 0;
   }
 `;
 
